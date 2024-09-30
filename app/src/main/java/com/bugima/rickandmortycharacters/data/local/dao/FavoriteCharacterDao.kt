b6f1859452144cbb48a72db15510242e7b54544b
bugima <macbugajski@gmail.com>
2024-09-30 19:37:52 +0200

    Dao fix

--- app/src/main/java/com/bugima/rickandmortycharacters/data/local/dao/FavoriteCharacterDao.kt
+++ app/src/main/java/com/bugima/rickandmortycharacters/data/local/dao/FavoriteCharacterDao.kt
@@ -1,11 +1,13 @@
 package com.bugima.rickandmortycharacters.data.local.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.bugima.rickandmortycharacters.data.local.entity.CharacterEntity
 
+@Dao
 interface FavoriteCharacterDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addToFavorites(character: CharacterEntity)
@@ -13,6 +15,6 @@ interface FavoriteCharacterDao {
     @Delete
     suspend fun removeFromFavorites(character: CharacterEntity)
 
-    @Query("SELECT id FROM favorite_characters")
-    suspend fun getAllFavoritesIds(): List<Int>
+    @Query("SELECT * FROM favorite_characters")
+    suspend fun getAllFavorites(): List<CharacterEntity>
 }
